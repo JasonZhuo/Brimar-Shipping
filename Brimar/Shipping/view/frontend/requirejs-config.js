5a19846b1b329c445da4d2a5d5e5cdd6a6738e80
JasonZhuo <linksforce.zyj@gmail.com>
2025-07-15 17:19:32 +0800

    Delete Brimar/Shipping directory

--- Brimar/Shipping/view/frontend/requirejs-config.js
+++
@@ -1,8 +0,0 @@
-var config = {
-    map: {
-        "*": {
-            "Brimar_Shipping/js/view/checkout/shipping/additional-options":
-                "Brimar_Shipping/js/view/checkout/shipping/additional-options",
-        },
-    },
-};
